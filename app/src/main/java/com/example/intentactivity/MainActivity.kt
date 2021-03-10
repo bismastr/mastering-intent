4410a368abdd9a2df69043f9c37b68a5aef949c5
Bisma Satria <bismasatrian@gmail.com>
2021-03-10 14:36:00 +0700

    adding MoveForResultActivity.kt and MoveWithObjectActivity.kt

--- app/src/main/java/com/example/intentactivity/MainActivity.kt
+++ app/src/main/java/com/example/intentactivity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.intentactivity
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnMoveActivity.setOnClickListener(this)
         val btnMoveData: Button = findViewById(R.id.btn_move_data)
         btnMoveData.setOnClickListener(this)
+        val btnMoveObject: Button = findViewById(R.id.btn_move_object)
+        btnMoveObject.setOnClickListener(this)
+        val btnDialNumber: Button = findViewById(R.id.btn_move_dial)
+        btnDialNumber.setOnClickListener(this)
     }
     override fun onClick(v: View?){
         when(v?.id){
@@ -27,6 +32,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveDataIntent.putExtra(MoveWithData.EXTRA_NAME, "Sarah")
                 moveDataIntent.putExtra(MoveWithData.EXTRA_AGE, 5)
                 startActivity(moveDataIntent)
+            }
+            R.id.btn_move_object -> {
+                val person = Person(
+                        "Dicoding Academy",
+                        5,
+                        "academy@gmail.com",
+                        "Bandung"
+                )
+                val moveObjectData = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveObjectData.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveObjectData)
+            }
+            R.id.btn_move_dial -> {
+                val phoneNumber = "085932990987"
+                val phoneDialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(phoneDialIntent)
+
             }
         }
     }
